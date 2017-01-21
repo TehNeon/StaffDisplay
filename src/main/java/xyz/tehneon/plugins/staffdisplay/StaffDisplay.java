39ef0d98e69b7bb60ec79093532bb302b5a8f587
Teh Neon <tehneon1212@gmail.com>
2017-01-21 13:43:08 -0500

    Initial Commit

---
+++ src/main/java/xyz/tehneon/plugins/staffdisplay/StaffDisplay.java
@@ -0,0 +1,17 @@
+package xyz.tehneon.plugins.staffdisplay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffDisplay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
