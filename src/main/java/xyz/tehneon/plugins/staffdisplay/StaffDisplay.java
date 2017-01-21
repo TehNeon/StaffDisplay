98fc5d2ca6c549ff5b83375efbc4cb7c0413c41f
Teh Neon <tehneon1212@gmail.com>
2017-01-21 13:51:02 -0500

    Created a config.yml with most if not all the configuration options that will be needed

--- src/main/java/xyz/tehneon/plugins/staffdisplay/StaffDisplay.java
+++ src/main/java/xyz/tehneon/plugins/staffdisplay/StaffDisplay.java
@@ -6,12 +6,11 @@ public final class StaffDisplay extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
