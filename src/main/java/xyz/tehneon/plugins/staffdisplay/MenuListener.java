e2106cd6a8687bf2983ae44c095afc7bf07f05a5
Teh Neon <tehneon1212@gmail.com>
2017-01-21 14:24:10 -0500

    Template for menu checking

--- src/main/java/xyz/tehneon/plugins/staffdisplay/MenuListener.java
+++ src/main/java/xyz/tehneon/plugins/staffdisplay/MenuListener.java
@@ -3,11 +3,12 @@ package xyz.tehneon.plugins.staffdisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 
 /**
  * @author TehNeon
  * @since 1/21/2017
- *
+ * <p>
  * The listener which will prevent any disturbances related to the menu.
  */
 public class MenuListener implements Listener {
@@ -20,5 +21,15 @@ public class MenuListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         // TODO: Check if the menu clicked has the title which is given inside the config
+    }
+
+    /**
+     * Checks if the provided inventory is the inventory which holds the Staff heads
+     *
+     * @param otherInventory Inventory which will be checked to be the menu
+     * @return Returns true if the provided menu is the same as the Staff heads menu
+     */
+    public boolean isTheMenu(Inventory otherInventory) {
+        return otherInventory.equals(null);
     }
 }
