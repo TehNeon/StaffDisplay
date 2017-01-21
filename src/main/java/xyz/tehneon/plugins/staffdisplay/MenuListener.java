f08c3a0b35619c84bbfd12965a9b596ecc330deb
Teh Neon <tehneon1212@gmail.com>
2017-01-21 14:10:51 -0500

    Created base plate for MenuListener

---
+++ src/main/java/xyz/tehneon/plugins/staffdisplay/MenuListener.java
@@ -0,0 +1,24 @@
+package xyz.tehneon.plugins.staffdisplay;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+/**
+ * @author TehNeon
+ * @since 1/21/2017
+ *
+ * The listener which will prevent any disturbances related to the menu.
+ */
+public class MenuListener implements Listener {
+    private StaffDisplay plugin;
+
+    public MenuListener(StaffDisplay plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        // TODO: Check if the menu clicked has the title which is given inside the config
+    }
+}
