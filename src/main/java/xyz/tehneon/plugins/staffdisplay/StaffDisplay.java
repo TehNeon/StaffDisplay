4208258e8f78fba868efa0a65a23b64fa7a0202a
Teh Neon <tehneon1212@gmail.com>
2017-01-21 14:44:43 -0500

    Add system to register the command, credits given to the person who showed a method similar to this

--- src/main/java/xyz/tehneon/plugins/staffdisplay/StaffDisplay.java
+++ src/main/java/xyz/tehneon/plugins/staffdisplay/StaffDisplay.java
@@ -1,6 +1,11 @@
 package xyz.tehneon.plugins.staffdisplay;
 
+import org.bukkit.command.CommandMap;
+import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.tehneon.plugins.staffdisplay.command.StaffDisplayCommand;
+
+import java.lang.reflect.Field;
 
 /**
  * @author TehNeon
@@ -14,6 +19,31 @@ public final class StaffDisplay extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Thanks to ItsSteve for the general concept of using the commandMap to register commands without using the plugin.yml
+        // Source: https://www.spigotmc.org/threads/small-easy-register-command-without-plugin-yml.38036/
+        if (getServer().getPluginManager() instanceof SimplePluginManager) {
+            CommandMap commandMap = null;
+
+            try {
+                Field commandMapField = SimplePluginManager.class.getDeclaredField("commandMap");
+                commandMapField.setAccessible(true);
+                commandMap = (CommandMap) commandMapField.get(getServer().getPluginManager());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if (commandMap != null) {
+                commandMap.register("staffdisplay", new StaffDisplayCommand(this));
+            } else {
+                getServer().getPluginManager().disablePlugin(this);
+                new RuntimeException("Your server software's PluginManager does not contain a commandMap so I cannot register a command. This may be due to the fact you might be running a custom Bukkit/Spigot version.");
+            }
+        } else {
+            getServer().getPluginManager().disablePlugin(this);
+            new RuntimeException("Your server software is running a PluginManager that is unrecognized. This may be due to the fact you might be running a custom Bukkit/Spigot version.");
+        }
+
+        // Register everything after the command incase the command cannot register it will disable the plugin
         menuBuilder = new MenuBuilder(this);
 
         getServer().getPluginManager().registerEvents(new MenuListener(this), this);
